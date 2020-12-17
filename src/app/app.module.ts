a28a49d6e262bec78b4dd7c758343814bc20cc97
Kiril Zafirov <kiril.zafirov@it-labs.com>
2020-12-17 15:15:07 +0100

    service worker to make the application pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeEntryModule } from './features/home-entry/home-entry.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [
     AppComponent
@@ -16,7 +18,8 @@ import { HomeEntryModule } from './features/home-entry/home-entry.module';
     BrowserAnimationsModule,
     AppRoutingModule,
     CoreModule,
-    HomeEntryModule
+    HomeEntryModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
